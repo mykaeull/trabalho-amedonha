89c05decc83060a43fc91e5a76c9bc70d1c1f045
brnamaia <74262856+brnamaia@users.noreply.github.com>
2021-03-10 10:42:32 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+// Bruna Raynara Maia Batista - 499257
+// Carlos Alberto Sampaio Sales Junior - 485181
+// Domingos Mykaeull Gomes Peres - 500594
+
+#include <stdio.h>
+
+int main () {
+
+    play_game();
+
+    return 0;
+}
